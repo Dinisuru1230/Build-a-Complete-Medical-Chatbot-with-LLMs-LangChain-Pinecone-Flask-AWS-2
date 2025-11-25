de9fdc60a9dce5c57f48c7d5fc6a823414a0fa47
Dinisuru1230 <dinisururangana1230@gmail.com>
2025-11-25 11:04:10 +0530

    folder structuter added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#Create directory
+mkdir -p src
+mkdir -p research
+
+#create files
+touch src/__intit__.py
+touch src/helprt.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "directory and files created successfully"
